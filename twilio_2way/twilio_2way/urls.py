11cf7d2ff24c81b3fc180ce7f33ce689ed409170
bernardndegwa <mrbernardndegwa@gmail.com>
2015-09-29 23:51:41 +0300

    First Unit test route and a dummy homepage

--- twilio_2way/twilio_2way/urls.py
+++ twilio_2way/twilio_2way/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from signup import views
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'twilio_2way.views.home', name='home'),
+    url(r'^$', 'signup.views.home_page', name='home_page'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+   # url(r'^admin/', include(admin.site.urls)),
 )
+
