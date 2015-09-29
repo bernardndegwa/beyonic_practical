e74cfad65eb60033192ace6fbc4208bb215c2eed
bernardndegwa <mrbernardndegwa@gmail.com>
2015-09-30 01:04:55 +0300

    Unit tests and models

--- twilio_2way/signup/tests.py
+++ twilio_2way/signup/tests.py
@@ -4,6 +4,8 @@ from django.http import HttpRequest
 from django.template.loader import render_to_string
 
 from signup.views import home_page 
+from signup.models import PhoneEmail
+
 
 class HomePageTest(TestCase):
 
@@ -17,3 +19,26 @@ class HomePageTest(TestCase):
         #self.assertTrue(response.content.startswith(b'<html>'))  
         #self.assertIn(b'<title>Sign Up Form</title>', response.content)  
         #self.assertTrue(response.content.strip().endswith(b'</html>')) 
+
+
+class PhoneEmailModelTest(TestCase):
+
+
+    def test_saving_and_retrieving_items(self):
+        first_item =PhoneEmail()
+        first_item.text = 'The first phoneemail is'
+        first_item.save()
+
+        second_item = PhoneEmail()
+        second_item.text = 'The second phoneemail is'
+        second_item.save()
+
+        saved_items = PhoneEmail.objects.all()
+        self.assertEqual(saved_items.count(), 2)
+
+        first_saved_item = saved_items[0]
+        second_saved_item = saved_items[1]
+        self.assertEqual(first_saved_item.text, 'The first phone email')
+        self.assertEqual(second_saved_item.text, 'The second phoneemail is')
+
+
