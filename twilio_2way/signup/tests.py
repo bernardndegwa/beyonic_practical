9d0bd264b153c80a9cacd5d384ed3db451d5fd5c
bernardndegwa <mrbernardndegwa@gmail.com>
2015-09-30 00:24:32 +0300

    Homepage now generated from template, added functional tests

--- twilio_2way/signup/tests.py
+++ twilio_2way/signup/tests.py
@@ -1,5 +1,8 @@
 from django.core.urlresolvers import resolve
 from django.test import TestCase
+from django.http import HttpRequest
+from django.template.loader import render_to_string
+
 from signup.views import home_page 
 
 class HomePageTest(TestCase):
@@ -7,3 +10,10 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')  
         self.assertEqual(found.func, home_page)
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()  
+        response = home_page(request)  
+        self.assertEqual(response.content.decode(), expected_html)
+        #self.assertTrue(response.content.startswith(b'<html>'))  
+        #self.assertIn(b'<title>Sign Up Form</title>', response.content)  
+        #self.assertTrue(response.content.strip().endswith(b'</html>')) 
